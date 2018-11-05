1d2bdfaaa7d90cea0824528c2015202ab817fa23
Martin Wilke <miwi@FreeBSD.org>
2018-11-05 12:01:10 +0000

    - Disable sql update

--- post_install.sh
+++ post_install.sh
@@ -25,14 +25,14 @@ echo "Database User: $USER"
 echo "Database Password: $PASS"
 
 # Configure mysql
-mysql -u root <<-EOF
-UPDATE mysql.user SET Password=PASSWORD('${PASS}') WHERE User='root';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
-CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASS}';
-GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';
-FLUSH PRIVILEGES;
-EOF
+#mysql -u root <<-EOF
+#UPDATE mysql.user SET Password=PASSWORD('${PASS}') WHERE User='root';
+#DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+#DELETE FROM mysql.user WHERE User='';
+#DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
+#CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASS}';
+#GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost' WITH GRANT OPTION;
+#GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';
+#FLUSH PRIVILEGES;
+#EOF
 
