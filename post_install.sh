440f55f21c8f6d909a1279e5fe83635ba2b3d5f8
Martin Wilke <miwi@FreeBSD.org>
2018-11-05 18:34:42 +0800

    Zoneminder Init
    - Enable Services
    - Prepare Database
    -

--- post_install.sh
+++ post_install.sh
@@ -1,1 +1,38 @@
 
+#Enable nginx
+sysrc -f /etc/rc.conf nginx_enable="YES"
+#Enable MySQL
+sysrc -f /etc/rc.conf mysql_enable="YES"
+#Enable fcgi_wrapper for nginx
+sysrc -f /etc/rc.conf fcgiwrap_enable="YES"
+sysrc -f /etc/rc.conf fcgiwrap_user="www"
+sysrc -f /etc/rc.conf fcgiwrap_flags="-c 4"
+#Enable PHP
+sysrc -f /etc/rc.conf php_fpm_enable="YES"
+
+# Database Setup
+USER="dbadmin"
+DB="ZMDB"
+
+# Save the config values
+echo "$DB" > /root/dbname
+echo "$USER" > /root/dbuser
+export LC_ALL=C
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > /root/dbpassword
+PASS=`cat /root/dbpassword`
+
+echo "Database User: $USER"
+echo "Database Password: $PASS"
+
+# Configure mysql
+mysql -u root <<-EOF
+UPDATE mysql.user SET Password=PASSWORD('${PASS}') WHERE User='root';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
+CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASS}';
+GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
