240d94119d8b69beacfb2d48d8906195d9049a28
Rafal Lukawiecki <rafal@projectbotticelli.com>
2020-04-19 12:37:19 +0100

    Fixing iocage poor treatment of newlines at the end of variable values

--- overlay/usr/local/bin/pluginget.sh
+++ overlay/usr/local/bin/pluginget.sh
@@ -6,36 +6,38 @@ logger -t pluginget "Reading $1"
 case $1 in
     httpport)
 	# If multiple non-SSL ports are configured, return the first one only
+	# xargs and printf used to ensure no newline at the end, which would
+	# otherwise confuse iocage's adminportal placeholder substitutions
         sed -En -e '/listen/Is/listen[[:blank:]]*([[:digit:]]+)[[:blank:]]*;/\1/Ip;' \
-            /usr/local/etc/nginx/conf.d/zoneminder.conf | head -n 1
+            /usr/local/etc/nginx/conf.d/zoneminder.conf | head -n 1 | xargs -0 printf "%s"
         ;;
     httpsport)
 	# If multiple SSL ports are configured, return the first one only
         sed -En -e '/listen/Is/listen[[:blank:]]*([[:digit:]]+)[[:blank:]]*ssl[[:blank:]]*;/\1/Ip;' \
-            /usr/local/etc/nginx/conf.d/zoneminder.conf | head -n 1
+            /usr/local/etc/nginx/conf.d/zoneminder.conf | head -n 1 | xargs -0 printf "%s"
         ;;
     sslcert)
 	# If multiple certificates have been set, return the last one
         sed -En -e '/ssl_certificate/Is/ssl_certificate[[:blank:]]*([^; ]+)[[:blank:]]*;/\1/Ip;' \
-            /usr/local/etc/nginx/conf.d/zoneminder.conf | tail -n 1
+            /usr/local/etc/nginx/conf.d/zoneminder.conf | tail -n 1 | xargs -0 printf "%s"
         ;;
     sslkey)
 	# If multiple private key files have been set, return the last one
         sed -En -e '/ssl_certificate_key/Is/ssl_certificate_key[[:blank:]]*([^; ]+)[[:blank:]]*;/\1/Ip;' \
-            /usr/local/etc/nginx/conf.d/zoneminder.conf | tail -n 1
+            /usr/local/etc/nginx/conf.d/zoneminder.conf | tail -n 1 | xargs -0 printf "%s"
         ;;
     adminprotocol)
         if [ "$(pluginget.sh httpsport)" -a -e "$(pluginget.sh sslcert)" -a -e "$(pluginget.sh sslkey)" ]; then
-            echo "https"
+            printf "https"
         else
-            echo "http"
+            printf "http"
         fi
         ;;
     adminport)
         if [ "$(pluginget.sh httpsport)" -a -e "$(pluginget.sh sslcert)" -a -e "$(pluginget.sh sslkey)" ]; then
-            pluginget.sh httpsport
+            printf "$(pluginget.sh httpsport)"
         else
-            pluginget.sh httpport
+            printf "$(pluginget.sh httpport)"
         fi
         ;;
     *)
--- overlay/usr/local/bin/pluginset.sh
+++ overlay/usr/local/bin/pluginset.sh
@@ -9,7 +9,7 @@ case $1 in
         sed -E -i '' -e '/listen[[:blank:]]*[[:digit:]]+[[:blank:]]*;/Id;' \
             /usr/local/etc/nginx/conf.d/zoneminder.conf
         # Insert a new listen directive
-        sed -E -i '' -e "/server/Ia \\
+        sed -E -i '' -e "/server[[:blank:]]+{/Ia \\
     listen $2;" \
             /usr/local/etc/nginx/conf.d/zoneminder.conf
         ;;
@@ -18,7 +18,7 @@ case $1 in
         sed -E -i '' -e '/listen[[:blank:]]*[[:digit:]]+[[:blank:]]*ssl[[:blank:]]*;/Id;' \
             /usr/local/etc/nginx/conf.d/zoneminder.conf
         # Insert a new listen directive
-        sed -E -i '' -e "/server/Ia \\
+        sed -E -i '' -e "/server[[:blank:]]+{/Ia \\
     listen $2 ssl;" \
             /usr/local/etc/nginx/conf.d/zoneminder.conf
         ;;
@@ -27,7 +27,7 @@ case $1 in
         sed -E -i '' -e '/ssl_certificate[[:blank:]]+.*;/Id;' \
             /usr/local/etc/nginx/conf.d/zoneminder.conf
         # Insert a new ssl_certificate line
-        sed -E -i '' -e "/server/Ia \\
+        sed -E -i '' -e "/server[[:blank:]]+{/Ia \\
     ssl_certificate $2;" \
             /usr/local/etc/nginx/conf.d/zoneminder.conf
         ;;
@@ -36,7 +36,7 @@ case $1 in
         sed -E -i '' -e '/ssl_certificate_key[[:blank:]]+.*;/Id;' \
             /usr/local/etc/nginx/conf.d/zoneminder.conf
         # Insert a new ssl_certificate_key line
-        sed -E -i '' -e "/server/Ia \\
+        sed -E -i '' -e "/server[[:blank:]]+{/Ia \\
     ssl_certificate_key $2;" \
             /usr/local/etc/nginx/conf.d/zoneminder.conf
         ;;
